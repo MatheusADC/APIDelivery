d8577ad4d06c92b595958562e8d9d3bae2ee32b0
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-08-21 14:10:14 -0300

    add: src

---
+++ src/models/repository/orders_repository.py
@@ -0,0 +1,22 @@
+class OrdersRepository:
+    def __init__(self, db_connection) -> None:
+        self.__collection_name = "orders"
+        self.__db_connection = db_connection
+
+    def insert_document(self, document: dict) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.insert_one(document)
+
+    def insert_list_of_documents(self, list_of_documents: list) -> None:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        collection.insert_many(list_of_documents)
+
+    def select_many(self, doc_filter: dict) -> list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(doc_filter)
+        return data
+    
+    def select_one(self, doc_filter: dict) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        response = collection.find_one(doc_filter)
+        return response
